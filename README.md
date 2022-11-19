e3c7cd975baeadeb23b6f1bc34df5ecabf5d3391
Jaisen Mathai <jaisen@jmathai.com>
2022-11-18 21:40:32 -0800

    Working version

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# synology-photos-reindex
+# Reindex Synology Photos
+
 Simple script to call Synology Photo's reindex API that can be scheduled to run periodically.
+
+## Instructions
+
+### Clone the repository.
+
+```
+git clone https://github.com/jmathai/synology-photos-reindexer.git
+```
+
+### Change into the directory.
+
+```
+cd synology-photos-reindexer
+```
+
+### Create virtual environment (optional but recommended)
+
+```
+python3 -m venv .env
+source .env/bin/activate
+```
+
+### Install dependencies.
+
+```
+pip3 install -r requirements.txt
+```
+
+### Export environment variables.
+
+```
+export SYNO_USERNAME=<your synology username>
+export SYNO_PASSWORD=<your synology password>
+export SYNO_HOSTNAME=<your synology hostname or ip address>
+```
+
+### Run the script.
+
+```
+./main.py
+```
+
+## Schedule the script
+
+Schedule this to run using Synology's Task scheduler which can be found in the Control Panel.
+
+From the Task Scheduler click Create -> Scheduled Task -> User defined script.
+
+On the Task Settings tab you'll use the following commands from above customized with your values.
+
+```
+export SYNO_USERNAME=<your synology username>
+export SYNO_PASSWORD=<your synology password>
+export SYNO_HOSTNAME=<your synology hostname or ip address>
+cd <path to git repo>
+source .env/bin/activate
+./main.py
+```
---
+++ main.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+import os
+import requests
+
+def call_api(api, method, version, **kwargs):
+    hostname = os.environ['SYNO_HOSTNAME']
+
+    url = 'https://{}:5001/webapi/query.cgi'.format(hostname)
+    # https://stackoverflow.com/a/66464593
+    data = kwargs
+    data['api'] = api
+    data['method'] = method
+    data['version'] = version
+    return requests.post(url, verify=False, data=data)
+
+def login():
+    username = os.environ['SYNO_USERNAME']
+    password = os.environ['SYNO_PASSWORD']
+
+    res_login = call_api('SYNO.API.Auth', 'login', '3', account=username, passwd=password)
+    res_login_json = res_login.json()
+    if('data' not in res_login_json):
+        exit('Login failed')
+
+    return res_login_json['data']['sid']
+
+def reindex(sid):
+    res_reindex = call_api('SYNO.Foto.Index', 'reindex', '1', _sid=sid)
+    if('data' not in res_reindex):
+        exit('Reindex failed')
+
+    return True
+
+
+if(__name__ == '__main__'):
+    if('SYNO_HOSTNAME' not in os.environ):
+        exit('Synology hostname (SYNO_HOSTNAME) not in environment')
+    if('SYNO_USERNAME' not in os.environ):
+        exit('Synology username (SYNO_USERNAME) not in environment')
+    if('SYNO_PASSWORD' not in os.environ):
+        exit('Synology password (SYNO_PASSWORD) not in environment')
+
+    login_sid = login()
+    res_reindex = reindex(login_sid)
+    print('Reindexed successfully')
